e98565b6e89ec5b3e29ea182d342053043c15abe
LAGHZALI taha <mrtahalz@gmail.com>
2022-02-11 11:42:08 +0100

    decide to use chakra ui

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { chakra, Box, Flex, useColorModeValue, Link } from "@chakra-ui/react";
+
 
 const Home: NextPage = () => {
   return (
@@ -17,10 +19,60 @@ const Home: NextPage = () => {
           Welcome to the movies explorer
         </h1>
           <div className={styles.grid}>
-              <a className={styles.card}>
-                  <h2>Movie title</h2>
-                  <p>Movie description.</p>
-              </a>
+              <Box
+                  bg={useColorModeValue("white", "gray.800")}
+                  mx={{ lg: 8 }}
+                  display={{ lg: "flex" }}
+                  maxW={{ lg: "5xl" }}
+                  shadow={{ lg: "lg" }}
+                  rounded={{ lg: "lg" }}
+              >
+                  <Box w={{ lg: "50%" }}>
+                      <Box
+                          h={{ base: 64, lg: "full" }}
+                          rounded={{ lg: "lg" }}
+                          bgSize="cover"
+                          style={{
+                              backgroundImage:
+                                  "url('https://images.unsplash.com/photo-1593642532400-2682810df593?ixlib=rb-1.2.1&ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80')",
+                          }}
+                      ></Box>
+                  </Box>
+
+                  <Box py={12} px={6} maxW={{ base: "xl", lg: "5xl" }} w={{ lg: "50%" }}>
+                      <chakra.h2
+                          fontSize={{ base: "2xl", md: "3xl" }}
+                          color={useColorModeValue("gray.800", "white")}
+                          fontWeight="bold"
+                      >
+                          Build Your New{" "}
+                          <chakra.span color={useColorModeValue("brand.600", "brand.400")}>
+                              Idea
+                          </chakra.span>
+                      </chakra.h2>
+                      <chakra.p mt={4} color={useColorModeValue("gray.600", "gray.400")}>
+                          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem
+                          modi reprehenderit vitae exercitationem aliquid dolores ullam
+                          temporibus enim expedita aperiam mollitia iure consectetur dicta
+                          tenetur, porro consequuntur saepe accusantium consequatur.
+                      </chakra.p>
+
+                      <Box mt={8}>
+                          <Link
+
+                              bg="gray.900"
+                              color="gray.100"
+                              px={5}
+                              py={3}
+                              fontWeight="semibold"
+                              rounded="lg"
+                              _hover={{ bg: "gray.800" }}
+                          >
+                              Start Now
+                          </Link>
+                      </Box>
+                  </Box>
+              </Box>
           </div>
 
       </main>
